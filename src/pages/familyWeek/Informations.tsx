9fbd264d9a802a62adaae1bfa7d84e5a22be5c00
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-08-08 08:23:28 -0300

    changes in ui of pages

--- src/pages/familyWeek/Informations.tsx
+++ src/pages/familyWeek/Informations.tsx
@@ -1,4 +1,4 @@
-import { CiAlarmOn, CiCalendar } from 'react-icons/ci';
+import { CiAlarmOn, CiCalendar, CiLocationOn } from 'react-icons/ci';
 import { Link } from 'react-router-dom';
 import { FrequentlyAsked } from '../../components/visitorRegistration/FrequentlyAsked';
 
@@ -7,17 +7,18 @@ export default function Informations() {
     <main className='p-4 h-full bg-stone-200'>
       <div className=' max-h-[100%] overflow-y-auto flex flex-col px-6 py-8 bg-white rounded-md'>
         <section>
-          <h1 className='font-bold mb-2'>O que é a Semana da Família?</h1>
-          <p className='text-sm min-[380px]:text-base'>
-            É um evento designado aos familiares dos colaboradores do grupo
-            Pedertractor & Tractorcomponents...
+          <h1 className='font-bold mb-2'>
+            O que é a Semana da Família no grupo Pedertractor?
+          </h1>
+          <p className=' text-justify text-sm min-[380px]:text-base'>
+            Nosso grupo desenvolve anualmente um evento para unir os integrantes
+            das famílias dos colaboradores e proporcionando um dia diferente,
+            conseguindo mostrar como é o funcionamento da nossa empresa.
           </p>
         </section>
 
         <section className='my-6'>
-          <h2 className='font-bold mb-2 min-[380px]:text-lg'>
-            Datas e horários do evento
-          </h2>
+          <h2 className='font-bold mb-2 min-[380px]:text-lg'>Informativos</h2>
           <p className='flex items-center gap-x-2 h-8 text-sm min-[380px]:text-base'>
             <CiCalendar size={24} />
             <span>16, 17, 18, 19 e 20 de Dezembro de 2024</span>
@@ -26,7 +27,12 @@ export default function Informations() {
             <CiAlarmOn size={22} />
             07:30h - 10:30h - 13:00h
           </p>
+          <p className='flex items-center mt-2 gap-x-2 h-8 text-sm min-[380px]:text-base'>
+            <CiLocationOn size={22} />
+            Pedertractor, Pederneiras-SP
+          </p>
         </section>
+        <CiLocationOn />
 
         <FrequentlyAsked />
 
